fb85eebffdddf79e325c654a99813d07f82ab14a
kk3131 <liuf979@gmail.com>
2024-06-25 17:38:14 +0800

    第六關實作

--- assets/js/scene3_1.js
+++ assets/js/scene3_1.js
@@ -19,9 +19,9 @@ class Scene3_1 extends Phaser.Scene {
     const b = Math.floor(Math.random() * 99 + 1);
     let question = 0;
     if (a > b) {
-      question = Math.floor(Math.random() * (a - b) + (b + 1));
+      question = Math.floor(Math.random() * (a - b - 1) + (b + 1));
     } else {
-      question = Math.floor(Math.random() * (b - a) + (a + 1));
+      question = Math.floor(Math.random() * (b - a - 1) + (a + 1));
     }
 
     const aContainer = this.add.container(150, 200);
