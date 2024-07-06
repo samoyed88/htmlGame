d018f6bc6d78c90ae9e6dd8ed8ef00ebe713f3a1
samoyed88 <12130383@me.mcu.edu.tw>
2024-07-06 13:50:52 +0800

    第 4 關說明介面實作

---
+++ assets/js/introduce4.js
@@ -0,0 +1,30 @@
+class Introduce4 extends Phaser.Scene {
+  constructor() {
+    super({ key: "Introduce4" });
+    this.button = null;
+    this.start = null;
+  }
+
+  preload() {
+    this.load.image("ins4", "assets/img/ins3.png");
+    this.load.image("next", "assets/img/下一步.png");
+  }
+
+  create() {
+    this.add.image(0, 0, "ins3").setOrigin(0, 0); //將中心點訂為左上角
+    // 加載 "下一步" 圖片並設置為可點擊
+    this.button = this.add
+      .image(900, 900, "next")
+      .setScale(0.47)
+      .setOrigin(0, 0)
+      .setInteractive();
+
+    // 添加點擊事件來切換到 Scene1
+    this.button.on("pointerup", () => {
+      // 刪除背景
+      //this.background.destroy();
+      // 切換到 Scene2
+      this.scene.start("Scene4");
+    });
+  }
+}
