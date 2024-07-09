0c4f3f4017af2ac1a1f59051ddf40e8727d27dac
samoyed88 <12130383@me.mcu.edu.tw>
2024-07-09 16:49:48 +0800

    API測試(start.js)

--- assets/js/start.js
+++ assets/js/start.js
@@ -6,46 +6,19 @@ class Start extends Phaser.Scene {
   preload() {
     // 加载游戏所需资源
     this.load.image("start", "assets/img/start.png");
-<<<<<<< HEAD
-    this.load.image("back-start", "assets/img/background-start.png");
-  }
-
-  create() {
-    this.add.image(0, 0, "back-start").setOrigin(0, 0);
-=======
     // 加载其他必要资源
   }
 
   create() {
     // 创建开始游戏按钮
->>>>>>> 2b38a78751fe0fccb40f7556563c7d6ff50a3e89
     this.add
-      .image(950, 650, "start")
-      .setScale(0.8) //0.35倍
+      .image(530, 278, "start")
+      .setScale(0.47)
       .setInteractive({ useHandCursor: true })
       .on("pointerup", () => {
         this.startGame();
       });
 
-<<<<<<< HEAD
-    this.illustrate = this.add
-      .image(950, 750, "illustrate")
-      .setScale(0.65)
-      .setInteractive({ useHandCursor: true })
-      .on("pointerup", () => {
-        this.gameStart2();
-      });
-  }
-
-  gameStart() {
-    this.scene.start("Preface");
-  }
-  gameStart2() {
-    this.scene.start("instructions");
-  }
-
-  update() {}
-=======
     // 添加一个按钮来测试API请求
     this.add
       .text(400, 500, "测试API", { fontSize: "32px", fill: "#fff" })
@@ -93,5 +66,4 @@ class Start extends Phaser.Scene {
       console.error("API请求错误:", error);
     }
   }
->>>>>>> 2b38a78751fe0fccb40f7556563c7d6ff50a3e89
 }
